14820f668dd5a353c3c331ff976f2d9d27cecbd5
gemmaturon <gemma@ersilia.io>
2024-11-09 23:03:49 +0100

    preds wip

--- scripts/02_run_ersilia_models.sh
+++ scripts/02_run_ersilia_models.sh
@@ -1,8 +1,17 @@
-filename="data/models_to_run.txt"
+filename="../data/models_to_run.txt"
+
+ersilia -v fetch eos2r5a --from_dockerhub
+ersilia -v fetch eos7d58 --from_dockerhub
+ersilia -v fetch eos2ta5 --from_dockerhub
+ersilia -v fetch eos9f6t --from_dockerhub
+ersilia -v fetch eos3nn9 --from_dockerhub
+ersilia -v fetch eos4e40 --from_dockerhub
+ersilia -v fetch eos7kpb --from_dockerhub
+ersilia -v fetch eos4zfy --from_dockerhub
 
 while IFS= read -r MODEL_ID; do
     echo "$MODEL_ID"
     ersilia serve "$MODEL_ID"
-    ersilia run -i data/all_smiles.csv -o results/intermediate/"$MODEL_ID".csv
+    ersilia run -i ../data/all_smiles.csv -o ../results/anpdb_intermediate/"$MODEL_ID".csv
     ersilia close
 done < "$filename"
