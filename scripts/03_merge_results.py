f172784e63078e0bb7c746754583ba60bbd5e524
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2024-11-11 21:43:44 +0100

    all tables

--- scripts/03_merge_results.py
+++ scripts/03_merge_results.py
@@ -16,9 +16,10 @@ df = pd.read_csv(os.path.join(ROOT, "..", "data", "all_molecules.tsv"), sep="\t"
 def columns_renamer(columns, model_id):
     rename = {}
     for c in columns:
+        v = None
         if c == "value" and model_id == "eos3nn9":
             v = "pic50"
-        else:
+        if v is None:
             v = c.lower()
             v = v.replace(" ", "_")
             v = v.replace("-", "_")
@@ -34,8 +35,11 @@ def load_model_results_file(model_id):
         return None
     df = pd.read_csv(file_name)
     columns = list(df.columns)[2:]
-    # filter for the admet model
     columns = [c for c in columns if "drugbank_approved_percentile" not in c]
+    if model_id == "eos7kpb":
+        print("Skipping some columns for the H3D model")
+        columns = [c for c in columns if "_norm" not in c]
+        columns = [c for c in columns if c.startswith("pf") or c.startswith("mtb")]
     df = df[columns]
     df = df.rename(columns=columns_renamer(columns, model_id), inplace=False)
     return df
@@ -49,3 +53,13 @@ for model_id in model_ids:
     df = pd.concat([df, df_], axis=1)
 
 df.to_csv(os.path.join(ROOT, "..", "results", "anpdb_annotated.tsv"), sep="\t", index=False)
+
+print("Saving individual results")
+columns = list(df.columns)
+
+for model_id in model_ids:
+    subcols = [x for x in columns if x.endswith(model_id)]
+    if len(subcols) == 0:
+        continue
+    subcols = columns[:3] + subcols
+    df[subcols].to_csv(os.path.join(ROOT, "..", "results", "by_model", "{0}.csv".format(model_id)), index=False)
