acba16c208ff854bcf83ce0cd8dbae876351abc8
gemmaturon <gemma@ersilia.io>
2024-11-11 12:52:25 +0100

    preds wip

--- scripts/02_run_ersilia_models.sh
+++ scripts/02_run_ersilia_models.sh
@@ -1,9 +1,5 @@
 filename="../data/models_to_run2.txt"
 
-ersilia -v fetch eos9f6t --from_dockerhub
-ersilia -v fetch eos7kpb --from_dockerhub
-ersilia -v fetch eos9gg2 --from_dockerhub
-
 while IFS= read -r MODEL_ID; do
     echo "$MODEL_ID"
     ersilia serve "$MODEL_ID"
