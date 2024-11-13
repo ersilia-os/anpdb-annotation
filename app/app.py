76311a06e8780573c4642c911805430e7415bf42
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2024-11-13 14:33:17 +0100

    second version containing plots

--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
 import os
 import json
 import pandas as pd
+import numpy as np
 import streamlit as st
+from scipy.stats import gaussian_kde
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 RESULTS_DIR = os.path.abspath(os.path.join(ROOT, "..", "results"))
@@ -34,10 +36,22 @@ def load_model_categories():
         data = json.load(f)
     return data
 
+@st.cache_data
+def convert_df(df):
+    return df.to_csv(index=False).encode('utf-8')
+
+@st.cache_data
+def read_drugbank_eos9gg2_data():
+    df = pd.read_csv(os.path.join(DATA_DIR, "drugbank_smiles.csv"))
+    df = pd.concat([df, pd.read_csv(os.path.join(DATA_DIR, "drugbank_eos9gg2.csv"))], axis=1)
+    return df
+
 df = load_results_data()
 models_metadata = load_models_metadata()
 model_categories = load_model_categories()
 model_readmes = load_models_readmes()
+csv = convert_df(df)
+db_eos9gg2 = read_drugbank_eos9gg2_data()
 
 available_model_ids = [x.split("_")[-1] for x in list(df.columns)[3:]]
 
@@ -45,9 +59,20 @@ available_model_ids = [x.split("_")[-1] for x in list(df.columns)[3:]]
 
 # Sidebar
 
+st.sidebar.image(os.path.join(ROOT, "..", "assets", "Ersilia_Brand.png"), width=200)
+
 st.sidebar.header("About this app")
 st.sidebar.markdown("This app was developed as part of a **capacity building workshop** on AI/ML for drug discovery in **Buea, Cameroon** in November 2024. The tool is designed to help researchers explore the **African Natural Products Database** (ANPDB) annotated with predictions from the **Ersilia Model Hub**.")
 st.sidebar.markdown("To reproduce this code or run the app locally, please visit the [anpdb-annotation](https://github.com/ersilia-os/anpdb-annotation) GitHub repository.")
+
+st.sidebar.download_button(
+"Download all annotations as CSV file",
+csv,
+"anpdb_ersilia_annotation.csv",
+"text/csv",
+key='download-csv'
+)
+
 st.sidebar.header("About ANPDB")
 st.sidebar.markdown("The [African Natural Products Database](https://african-compounds.org/anpdb/) is a database of natural product compounds found in Africa. It is maintained by the [University of Buea Centre for Drug Discovery](https://www.ub-cedd.org/).")
 
@@ -62,59 +87,87 @@ st.sidebar.info("The Ersilia Model Hub is intended for research purposes. Please
 st.title("ANPDB annotated with the Ersilia Model Hub")
 
 
-def create_model_checkbox(col, category, value=None):
-    title = category
+def create_model_checkbox(col, category):
+    title = "Select a model from the {0} category".format(category)
     model_ids = model_categories[category]
     display_options = ["[{0}](https://github.com/ersilia-os/{0}): {1}".format(model_id, models_metadata[model_id]["title"]) for model_id in model_ids]
-    if value is not None:
-        value = display_options[value]
-    selected_options = col.pills(options=display_options, label=title, selection_mode="multi", default=value)
-    return [x.split("[")[1].split("]")[0] for x in selected_options]
+    value = display_options[0]
+    selected_option = col.pills(options=display_options, label=title, selection_mode="single", default=value)
+    if selected_option is None:
+        return None
+    else:
+        return selected_option.split("[")[1].split("]")[0]
 
 st.header(":robot_face: Select models to display!")
 
-chem_model_ids = create_model_checkbox(st, "Chemical Properties")
-adme_model_ids = create_model_checkbox(st, "ADMET")
-bact_model_ids = create_model_checkbox(st, "Bioactivity")
-proj_model_ids = create_model_checkbox(st, "2D projections")
+category = st.pills("Select a category", list(model_categories.keys()), selection_mode="single", default=None)
+if category is not None:
 
-selected_model_ids = chem_model_ids + adme_model_ids + bact_model_ids + proj_model_ids
-selected_model_ids = [x for x in selected_model_ids if x in available_model_ids]
+    sel_model_id = create_model_checkbox(st, category)
 
-st.header(":herb: Explore the data")
+    if sel_model_id is not None:
+        readme = model_readmes[sel_model_id]
+        description = readme.split("# ")[1].split("## ")[0].split("\n")[1:]
+        description = "\n".join(description).strip("\n").replace("\n", "").replace("#", "")
+        publication_url = readme.split("[Publication]")[1].split("(")[1].split(")")[0]
+        code_url = readme.split("[Source Code]")[1].split("(")[1].split(")")[0]
+        st.markdown(description)
+        st.markdown("* [Publication]({0})\n* [Code](code_url)".format(publication_url))
 
-columns_to_keep = list(df.columns)[:3] + [x for x in list(df.columns[3:]) if x.split("_")[-1] in selected_model_ids]
-df = df[columns_to_keep]
-st.dataframe(df)
+        st.header(":herb: Explore the data")
+        
+        selected_model_ids = [sel_model_id]
 
-@st.cache_data
-def convert_df(df):
-   return df.to_csv(index=False).encode('utf-8')
+        columns_to_keep = list(df.columns)[:3] + [x for x in list(df.columns[3:]) if x.split("_")[-1] in selected_model_ids]
+        df = df[columns_to_keep]
+        st.dataframe(df)
 
-csv = convert_df(df)
+        st.header(":eyes: Visualize the data")
 
-st.download_button(
-   "Download as CSV file",
-   csv,
-   "anpdb_ersilia_annotation.csv",
-   "text/csv",
-   key='download-csv'
-)
+        if sel_model_id != "eos9gg2":
+            visualization_options = ["_".join(x.split("_")[:-1]) for x in columns_to_keep[3:]]
+            plot_type = "distribution"
+        else:
+            visualization_options = ["UMAP", "tSNE", "PCA 1 vs 2", "PCA 3 vs 4"]
+            plot_type = "scatter"
 
-st.header(":book: Learn more about the models")
-models = [x for k,v in model_categories.items() for x in v]
-options = ["{0}: {1}".format(model, models_metadata[model]["title"]) for model in models]
-selected_option = st.selectbox("Select a model", options, index=None)
-if selected_option is not None:
-    selected_model_id = selected_option.split(":")[0]
-else:
-    selected_model_id = None
+        selected_option = st.selectbox("What do you want to visualize?", visualization_options, index=0)
+        if plot_type == "distribution":
+            # Sample data
+            data = list(df[selected_option+"_"+sel_model_id])
+            # Create a Gaussian KDE
+            kde = gaussian_kde(data)
 
-if selected_model_id is not None:
-    readme = model_readmes[selected_model_id]
-    description = readme.split("# ")[1].split("## ")[0].split("\n")[1:]
-    description = "\n".join(description).strip("\n").replace("\n", "").replace("#", "")
-    publication_url = readme.split("[Publication]")[1].split("(")[1].split(")")[0]
-    code_url = readme.split("[Source Code]")[1].split("(")[1].split(")")[0]
-    st.markdown(description)
-    st.markdown("* [Publication]({0})\n* [Code](code_url)".format(publication_url))
+            # Define a range of values where you want to evaluate the KDE
+            x_values = np.linspace(min(data), max(data), 100)  # 100 points between min and max of data
+
+            # Get density values
+            density_values = kde(x_values)
+            dp = pd.DataFrame({selected_option: x_values, "density": density_values})
+            st.area_chart(dp, x=selected_option, y="density")
+        else:
+            if selected_option == "UMAP":
+                xcol = "umap_1"
+                ycol = "umap_2"
+            elif selected_option == "tSNE":
+                xcol = "tsne_1"
+                ycol = "tsne_2"
+            elif selected_option == "PCA 1 vs 2":
+                xcol = "pca_1"
+                ycol = "pca_2"
+            elif selected_option == "PCA 3 vs 4":
+                xcol = "pca_3"
+                ycol = "pca_4"
+            else:
+                xcol = None
+                ycol = None
+            dp = pd.DataFrame({xcol: df[xcol+"_"+sel_model_id], ycol: df[ycol+"_"+sel_model_id]})
+            dp["category"] = "anpdb"
+
+            db = pd.DataFrame({xcol: db_eos9gg2[xcol.replace("_", "-")], ycol: db_eos9gg2[ycol.replace("_", "-")]})
+            db["category"] = "drugbank"
+
+            dp = pd.concat([db, dp], axis=0)
+
+            st.scatter_chart(dp, x=xcol, y=ycol, color="category", width=1000, height=1000)
+            
